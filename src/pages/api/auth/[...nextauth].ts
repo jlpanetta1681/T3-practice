9d45b2ae72273679b032208eb12306838a0fad61
Joe Panetta <jlpanetta1681@icloud.com>
2022-08-10 19:03:06 -0400

    main-scaffolding: builds out the main frame and layout of the application

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,29 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+  ],
+   
+secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+};
+
+export default NextAuth(authOptions);
